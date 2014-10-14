49fba359934a399d2a477453fecc20a9c5ea761e
Diego Serrano <diegoserranoa@Diegos-MacBook-Pro.local>
2014-10-14 00:33:03 -0500

    reorder

---
+++ Demo/DSTranparentNavigationBar/DSNavigationBar.h
@@ -0,0 +1,16 @@
+//
+//  DSNavigationBar.h
+//  DSTranparentNavigationBar
+//
+//  Created by Diego Serrano on 10/13/14.
+//  Copyright (c) 2014 Diego Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSNavigationBar : UINavigationBar
+
+-(void)setNavigationBarWithColor:(UIColor *)color;
+-(void)setNavigationBarWithColors:(NSArray *)colours;
+
+@end
